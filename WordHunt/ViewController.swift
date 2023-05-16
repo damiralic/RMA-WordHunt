0cef6746a6c53a700c5013eee3dac1c3ffd677cd
Damir Alic <damir.alic11@gmail.com>
2023-05-16 13:11:35 +0200

    Registration

--- WordHunt/ViewController.swift
+++ WordHunt/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextInput: UITextField!
+    @IBOutlet weak var emailTextInput: UITextField!
+    @IBOutlet weak var passwordTextInput: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let placeholderText = NSAttributedString(string: usernameTextInput.placeholder!, attributes: [NSAttributedString.Key.foregroundColor:UIColor.white])
+        usernameTextInput.attributedPlaceholder = placeholderText
+        emailTextInput.attributedPlaceholder = placeholderText
+        passwordTextInput.attributedPlaceholder = placeholderText
     }
 
 
+    @IBAction func registerButton(_ sender: UIButton) {
+    }
 }
 
